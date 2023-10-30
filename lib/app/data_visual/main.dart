b3eb6920fc434931f75d4a956ba7023f4448f487
蔡凯歌 <caikaige.g@gmail.com>
2023-10-31 05:54:25 +0800

    数据可视化 滚动动画

--- lib/app/data_visual/main.dart
+++ lib/app/data_visual/main.dart
@@ -1,9 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'data_model.dart';
+import 'page/data_visual_1.dart';
 
 class DataVisualApp extends StatefulWidget {
   const DataVisualApp({super.key});
@@ -15,6 +13,7 @@ class DataVisualApp extends StatefulWidget {
 class _DataVisualAppState extends State<DataVisualApp> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive, overlays: []);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: <String, WidgetBuilder>{
@@ -60,96 +59,6 @@ class _DataVisualAppState extends State<DataVisualApp> {
             );
           },
           itemCount: 9, // 总的单元格数量
-        ),
-      ),
-    );
-  }
-}
-
-class DataVisualPage1 extends StatefulWidget {
-  const DataVisualPage1({super.key});
-
-  @override
-  State<DataVisualPage1> createState() => _DataVisualPage1State();
-}
-
-class _DataVisualPage1State extends State<DataVisualPage1> {
-  List<DataModel> jsonData = []; // 现在使用 DataModel 类型的列表
-
-  @override
-  void initState() {
-    super.initState();
-    loadJsonData();
-  }
-
-  Future<void> loadJsonData() async {
-    final String data = await rootBundle.loadString('asset/json/data0.json');
-    final dynamic jsonResult = json.decode(data);
-
-    if (jsonResult is Map && jsonResult.containsKey("result")) {
-      final List<dynamic> results = jsonResult["result"];
-      jsonData = results
-          .map((item) => DataModel.fromJson(item))
-          .toList(); // 使用 DataModel 类来映射数据
-    }
-
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: List<Widget>.generate(
-              jsonData.length,
-              (index) {
-                return SizedBox(
-                  width: MediaQuery.of(context).size.width / 3,
-                  child: CardItem(dataModel: jsonData[index]),
-                );
-              },
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class CardItem extends StatelessWidget {
-  final DataModel dataModel;
-
-  const CardItem({
-    super.key,
-    required this.dataModel,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 8.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.0), // 设置圆角半径
-      ),
-      child: Container(
-        width: 200,
-        margin: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Image.network(
-              dataModel.pic,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 8.0),
-            Text(dataModel.value),
-            Text(dataModel.title),
-            Text(dataModel.subtitle),
-          ],
         ),
       ),
     );
